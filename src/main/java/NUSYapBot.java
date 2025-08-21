95a339628229515552316b715dadd4de1de48d98
Dickson <dicksonlai66@gmail.com>
2025-08-21 11:16:48 +0800

    Add info-storing and retrieving function to NUSCYapBot

--- src/main/java/NUSYapBot.java
+++ src/main/java/NUSYapBot.java
@@ -7,25 +7,43 @@ public class NUSYapBot {
                          Hello! I'm NUSYapBot!
                          What can I do for you?
                         _________________________________
-                        
                          """;
 
         String end = """
-                        Goodbye. See you!
-                        _________________________________
+                     _________________________________
+                     Goodbye. See you!
+                     _________________________________
                      """;
         System.out.println(welcome);
         boolean flag = true;
+        String[] list = new String[100];
+        int pointer = 0;
         while (flag) {
             Scanner input = new Scanner(System.in);
             String answer = input.nextLine();
-            if (!answer.equals("bye")) {
-                System.out.println("> " + answer);
-            } else {
-                flag = false;
+
+            switch (answer) {
+                case "list":
+                    int i = 0;
+                    System.out.println("_________________________________");
+                    while (list[i] != null) {
+                        System.out.println(i+1 + ". " + list[i]);
+                        i++;
+                    }
+                    System.out.println("_________________________________");
+                    break;
+                case "bye":
+                    flag = false;
+                    System.out.println(end);
+                    break;
+                default:
+                    System.out.println("_________________________________" + "\n" +
+                                       "added: " + answer + "\n" +
+                                       "_________________________________");
+                    list[pointer] = answer;
+                    pointer++;
+                    break;
             }
         }
-
-        System.out.println(end);
     }
 }
